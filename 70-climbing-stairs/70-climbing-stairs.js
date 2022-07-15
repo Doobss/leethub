20c0b0eb74b06086fee73b321912e862ad858740
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-07-15 15:13:14 -0400

    Time: 96 ms (30.22%), Space: 42.2 MB (10.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -8,28 +8,9 @@ var climbStairs = function(n) {
     let sum = 1;
     while(n--) {
         let valueHolder = sum;
-        sum+=prev
+        sum+= prev
         prev = valueHolder
     }
+    
     return sum
 };
-
-
-    
-//     var countSteps = (stepsLeft = n) => {
-//         let numberOfSteps = 0;
-        
-//         if(!stepsLeft) {
-//             return 1
-//         }
-//         else {
-//             let nextStep = stepsLeft
-//             numberOfSteps += countSteps(nextStep - 1)
-//             nextStep--
-//             if(nextStep) {
-//                 numberOfSteps += countSteps(nextStep - 1)
-//             }
-//             return numberOfSteps
-//         }
-//     }
-//     return countSteps()
