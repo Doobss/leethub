db82e04049923fee90ff2fa0162eaae2c95d4c28
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-08-26 19:57:51 -0400

    Time: 5 ms (18.17%), Space: 5.6 MB (19.09%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.c
+++ 70-climbing-stairs/70-climbing-stairs.c
@@ -1,16 +1,3 @@
-// int visited[47];
-// void fill()
-// {
-//   int i = 47;
-//   while (i--) visited[i] = 0;
-//   return;
-// }
-    // total_calls++;
-    // visited[n]++;
-    // if (visited[46] != 0) fill();
-    // printf("n = %d v = %d t = %d s = %d \n", n, visited[n], total_calls, steps); 
-
-
 
 int climbStairs(int n) {
     int prev = 0, sum = 1, swap;
