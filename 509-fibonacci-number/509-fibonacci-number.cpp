23f2225368b26b03f875f5bb95706debc767d8c3
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-08-26 16:41:16 -0400

    Time: 6 ms (39.55%), Space: 5.8 MB (96.14%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n){
+        int last, current, sum;
+        sum = 0;
+        current = 1;
+        while (n--) 
+        {
+            last = sum;
+            sum += current;
+            current = last;
+        }
+        return sum;
+    }
+};
