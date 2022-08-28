b7af818dd1a84ae95bde96ce394ee58fb76b62e2
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-08-27 20:34:57 -0400

    Time: 5 ms (23.19%), Space: 6.1 MB (10.72%) - LeetHub

--- 198-house-robber/198-house-robber.c
+++ 198-house-robber/198-house-robber.c
@@ -14,20 +14,17 @@ int max(int a, int b) {
 
 
 int find_houses(int house_indx) {
+    if (house_indx > number_of_houses - 1) return 0;
     if (robbed[house_indx] != -1) return robbed[house_indx];
 
-    robbed[house_indx] = *(house_values + house_indx);
-    if (2 >=  (number_of_houses - house_indx)) {
-        return robbed[house_indx];
-    }
-    int house_max = max(find_houses(house_indx + 1), (robbed[house_indx] + find_houses(house_indx + 2)));
+    int house_max = max(*(house_values + house_indx), find_houses(house_indx + 1));
     int j = house_indx + 2;
     while ( number_of_houses > j ) {
-      house_max = max(house_max,  (robbed[house_indx] + find_houses(j)));
+      house_max = max(house_max,  (*(house_values + house_indx) + find_houses(j)));
       j++;
     }
     robbed[house_indx] = house_max;
-    return robbed[house_indx];
+    return house_max;
 }
 
 
@@ -36,5 +33,5 @@ int rob(int* nums, int numsSize) {
     fill();
     number_of_houses = numsSize;
     house_values = nums;
-    return max(find_houses(0), find_houses(1));;
+    return find_houses(0);
 }
