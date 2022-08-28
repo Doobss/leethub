8a5a57ee15f519bebba1a2b39b176cf79c4c7a1c
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-08-28 14:48:39 -0400

    Time: 575 ms (6.85%), Space: 7.4 MB (31.51%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.c
@@ -0,0 +1,82 @@
+#define MAX_VALUE 10001
+#define MAX(a, b)(((a) >= (b) ? (a) : (b)))
+
+int* store[MAX_VALUE]; 
+// after preprocess this is sorted matrix (m x 2, where m = unique indicies) 
+//   store[m][1] = index value
+//   store[m][0] = number of times this index occured 
+int store_length = MAX_VALUE;
+int* calculated;  // used to store previously calculated max values
+int initalized = 0; 
+
+
+void clear_store() {
+  int i = MAX_VALUE;
+  if (initalized) {
+    while(i--) {
+      store[i][0] = 0; // number of occurances
+      store[i][1] = i; // index to keep track after filtering down
+    }
+  } else {
+    initalized++;
+    while(i--) {
+      store[i] = (int*) calloc(2, sizeof(int));
+      store[i][1] = i;
+    }
+  }
+}
+
+
+
+int filter_store(int removed_int) { // filters out all store[m][0] = removed_int;
+    int new_length = 0;
+    for(int i = 0; store_length > i; i++) {
+      int* store_vals = store[i];
+        if (store_vals[0] != removed_int)  {
+            store[new_length][0] = store_vals[0];
+            store[new_length][1] = store_vals[1];
+            new_length++;
+        }
+    }
+    return new_length;
+}
+
+
+
+
+void preprocess(int* starting_array, int array_length) {
+  store_length = MAX_VALUE;
+  clear_store();
+  int i = array_length;
+  while (i--) {
+        store[starting_array[i]][0] += 1;
+  }
+  store_length = filter_store(0);
+  calculated = (int*) calloc((unsigned long) store_length, sizeof(int));
+  return;
+}
+
+
+int compute(int indx) {
+    
+  if(calculated[indx] != 0) return calculated[indx]; // return previous calculation
+
+  int current_max = 0;
+  int k = indx;
+  while(store_length > ++k) {
+    if (store[indx][1] + 1 != store[k][1]) {
+      current_max = MAX(current_max, compute(k));
+    }
+  }
+  calculated[indx] = (store[indx][0] * store[indx][1]) + current_max;
+  return calculated[indx];
+}
+
+
+int deleteAndEarn(int* nums, int numsSize) {
+  preprocess(nums, numsSize);
+  if (2 > store_length) return ( store[0][0] * store[0][1] );
+  // free(calculated);
+  return MAX(compute(0), compute(1));;
+}
+
