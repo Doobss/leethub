9bb0a224cb699982f9c3b6022c31242ca8df3496
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2022-08-28 22:03:25 -0400

    Time: 562 ms (7.91%), Space: 9 MB (14.39%) - LeetHub

--- 55-jump-game/55-jump-game.c
+++ 55-jump-game/55-jump-game.c
@@ -16,13 +16,15 @@ int can_jump_to(int from_indx, int to_indx) {
 int find_path(int indx) {
     if (indx > last_indx || 0 > indx) return 0;
     if (store[indx] !=  - 1) return store[indx];
-    store[indx] = can_jump_to(indx, last_indx);
-    for(int i = 1; jump_distances[indx] >= i && !store[indx]; i++) {
-        store[indx] = find_path(indx + i);
+    int max_jump = jump_distances[indx];
+    int found_path = can_jump_to(indx, last_indx);
+    store[indx] = found_path;
+    for(int i = 1; max_jump >= i && !found_path; i++) {
+        found_path = find_path(indx + i);
     }
-    return store[indx];
+    store[indx] = found_path;
+    return found_path;
 }
-
 bool canJump(int* nums, int numsSize) {
     if (2 > numsSize) return numsSize;
     total_distance = numsSize;
